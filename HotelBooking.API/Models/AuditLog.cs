2f2728e670dfbf2bb02f2060929a51042636ab10
Anusuba18 <anu18balu@gmail.com>
2026-04-07 11:56:17 +0530

    Added Models folder

--- HotelBooking.API/Models/AuditLog.cs
+++ HotelBooking.API/Models/AuditLog.cs
@@ -1,6 +1,25 @@
-﻿namespace HotelBooking.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelBooking.API.Models;
+
+[Table("AuditLogs")]
+public class AuditLog
 {
-    public class AuditLog
-    {
-    }
+    [Key]
+    public int Id { get; set; }
+
+    public int? UserId { get; set; }
+
+    [Required, MaxLength(200)]
+    public string Action { get; set; } = string.Empty;
+
+    public string? Details { get; set; }
+
+    [MaxLength(50)]
+    public string? IpAddress { get; set; }
+
+    public int? StatusCode { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
